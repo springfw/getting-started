366655a69755902b10fc87d880a6b50c765afc14
Duong Nguyen <duongnguyen@hypertech.com.vn>
2017-12-03 14:10:49 +0700

    Initital Getting Started project

---
+++ src/main/java/io/cobrafw/spring/controllers/IndexController.java
@@ -0,0 +1,38 @@
+package io.cobrafw.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String home() {
+        return "/home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "/user";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/about";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/403")
+    public String error403() {
+        return "/error/403";
+    }
+}
